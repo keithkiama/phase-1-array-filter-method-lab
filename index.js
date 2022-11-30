ed13931c6b1d0ff062b6e7a6673d5df476b649e8
keithkiama <keithkiama2003@gmail.com>
2022-11-30 17:17:49 +0300

    Completed Assignement

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 const findMatching = (array,string) =>{
   return array.filter((item) => item.toLowerCase() === string.toLowerCase())
 }
-console.log(findMatching(drivers,"sammy"))
+console.log(findMatching(drivers,"Bobby"))
 
-const fuzzyMatch = (array,string) =>{
+const fuzzyMatch = (array,string) =>{   
   return array.filter((item) => {
       if (item.startsWith(string)){
         return item
       }
   })
 }
-console.log(fuzzyMatch(drivers,"Bo"))
+console.log(fuzzyMatch(drivers,"Sa"))
 
 const matchName = (array,string) => {
   return array.filter((item) => item.name === string)
 }
-console.log(matchName(drivers,"Sarah"))
+console.log(matchName(drivers,"Sally"))

5725c289bdb9e13b8a225498f46bc5f725735b4d
keithkiama <keithkiama2003@gmail.com>
2022-11-30 11:09:41 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const findMatching = (array,string) =>{
+  return array.filter((item) => item.toLowerCase() === string.toLowerCase())
+}
+console.log(findMatching(drivers,"sammy"))
+
+const fuzzyMatch = (array,string) =>{
+  return array.filter((item) => {
+      if (item.startsWith(string)){
+        return item
+      }
+  })
+}
+console.log(fuzzyMatch(drivers,"Bo"))
+
+const matchName = (array,string) => {
+  return array.filter((item) => item.name === string)
+}
+console.log(matchName(drivers,"Sarah"))

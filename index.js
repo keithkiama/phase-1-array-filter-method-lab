8277c7feb75ced773c1febb3a29b6c9ade0af285
keithkiama <keithkiama2003@gmail.com>
2022-12-06 09:22:16 +0300

    Updated

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 // Code your solution here
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 
-const findMatching = (array,string) =>{
-  return array.filter((item) => item.toLowerCase() === string.toLowerCase())
+const findMatching = (drivers,name) =>{
+  return drivers.filter((item) => item.toLowerCase() === name.toLowerCase())
 }
 console.log(findMatching(drivers,"Bobby"))
 
-const fuzzyMatch = (array,string) =>{   
-  return array.filter((item) => {
-      if (item.startsWith(string)){
+const fuzzyMatch = (drivers,name) =>{   
+  return drivers.filter((item) => {
+      if (item.startsWith(name)){
         return item
       }
   })
 }
 console.log(fuzzyMatch(drivers,"Sa"))
 
-const matchName = (array,string) => {
-  return array.filter((item) => item.name === string)
+const matchName = (drivers,name) => {
+  return drivers.filter((item) => item.name === name)
 }
 console.log(matchName(drivers,"Sally"))
